32951057a2143e686d0af394c3f00d32215ce905
Maria Hansen <79523132+MariaMHansen@users.noreply.github.com>
2022-11-08 21:45:39 +0100

    tests

--- src/main/java/MethodsPractice.java
+++ src/main/java/MethodsPractice.java
@@ -1,16 +1,176 @@
+import java.util.Arrays;
+
 public class MethodsPractice {
+    /*
+       Welcome to assignment three,
+       Where you will practice with using arrays and methods.
+       You have already tried making methods in previous tasks,
+       where each step was a small program performing one function.
+       Methods are commonly used in programming,
+       because they allow to reuse small chunks of functionality
+       to build programs avoiding duplicated code.
+
+       ------------------------------------------------------------
+       Checking Passwords with Arrays
+       -------------------------------------------------------------
+       An array is a data structure commonly used to store multiple values in a single variable,
+       instead of declaring separate variables for each one of them.
+       For example all vowels in the English alphabet can be stored in this character array:
+       char[] vowels = {a, e, i, o, u};
+       This is very convenient for writing programs where some statement needs to be checked
+       for each item in a row.
+
+       In this exercise, you will practice using arrays for going through a password letter by letter
+       and checking which characters they contain to make a password strength evaluator.
+       Scroll down to STEP 1:
+       */
+
+
+    /**
+     * This is the Main method,
+     * Uncomment the lines of code for each method you implement
+     * to test your solution
+     * Experiment with entering your own attributes
+     */
     public static void main(String[] args) {
-        MethodsPractice.sayHello();
+        System.out.println("Step 1: passwordLengthChecker: " + passwordLengthChecker("Password123"));
+        System.out.println("Step 2: stringToCharArray: " + Arrays.toString(stringToCharArray("Password123")));
+        System.out.println("Step 3: passwordContainsUpperCase: " + passwordContainsUpperCase("Password123"));
+        System.out.println("Step 4: passwordContainsSymbolChecker: " + passwordContainsSymbolChecker("Pa$$word123",new char[]{'$','%','&'}));
+        System.out.println("Step 5: passfordStrengthEvaluator: " + passwordStrengthEvaluator("BaCdGzWZ$"));
     }
+
+    /**
+     *  STEP 1:
+     *     In many situations it might be necessary to count the amount of symbols in a string,
+     *     for example to check if a password consists of 8 or more letters,
+     *     which is a common security requirement for many login systems.
+     *     Make a method that takes a String an input,
+     *     checks how many letters it contains,
+     *     and returns true if the number is more or equal to 8.
+     *     Example: Using "password123" as input would return true
+     *     (We assume that the input is always valid)
+     * @param password String to count letters in
+     * @return  true if there are 8 or more letters, false if less
+     * Points: 2
+     * New skills: String length
+     * Difficulty: 1
+     */
+
+    public static boolean passwordLengthChecker(String password){
+        if(password.length()>=8){return true;}
+        return false;
+    }
+
+    /**
+     *  STEP 2:
+     *     Another useful method for programming with Strings,
+     *     is being able to go through a word letter by letter, to check something.
+     *     Make a method that converts a String to an array of characters.
+     *     We will need this for future steps.
+     *     Example: Using "hello" as input would return "[h, e, l, l, o]"
+     * @param str String to be converted
+     * @return  char [] consisting of letters from the word used as input
+     * Points: 2
+     * New skills: String toCharArray()
+     * Difficulty: 2
+     */
+    public static char[] stringToCharArray(String str){
+        char[] chars = str.toCharArray();
+        return chars;
+    }
+
+    /**
+     *  STEP 3:
+     *     Using uppercase letters makes passwords stronger.
+     *     Make a method that checks a password letter by letter,
+     *     and returns true if it contains upper case letters.
+     *     Example: Using "Password123" as input would return true, because it contains P
+     *     Tip! To print out an array to your console try: System.out.println(Arrays.toString(chars));
+     * @param password String to be checked for spaces
+     * @return  true if there are upper case letters in the input String, false if there are none
+     * Points: 3
+     * New skills: Iterating an array
+     * Difficulty: 3
+     */
+    public static boolean passwordContainsUpperCase(String password){
+        char [] letters = stringToCharArray(password);
+         for(char letter:letters){
+                 if (Character.isUpperCase(letter)){
+                      return true;
+                  }
+        }
+        return false;
+    }
+
+    /**
+     *  STEP 4:
+     *     Password strength increases if it contains special symbols,
+     *     such as %,&,£,$ etc.
+     *     Make a method that checks if a password contains any symbol
+     *     form an array of characters, which is also provided as input.
+     *     Example: Using ("Pa$$word123",new char[]{'$','%','&'}) as input would return true
+     *     Tip! Remember the nested loop exercise from the previous assignment
+     * @param password String to be checked for spaces
+     * @param symbols char array of special symbols to be checked
+     * @return  true if there is one of the symbols in the password, false if there are none
+     * Points: 3
+     * New skills: comparing two arrays
+     * Difficulty: 4
+     */
+    // check if it has an array of symbols ...
+   public static boolean passwordContainsSymbolChecker(String password, char[] symbols){
+       char[] letters = stringToCharArray(password);
+       for (char letter: letters) {
+          for(char symbol: symbols){
+              if(letter==symbol){
+                  return true;
+              }
+          }
+       }
+        return false;
+   }
+
+    /**
+     *  STEP 5:
+     *     Finally, all the methods are ready to create the password StrengthEvaluator!
+     *     Make a method that checks password strength after using the following rules,
+     *     "weak" - Password length is under 8 characters
+     *     "medium" - Password length is over 8 characters, and it contains special symbols
+     *     "strong" - Password length is over 8 characters, and it contains special symbols and upper case letters
+     *     Reusing the methods you have created in steps 1, 3 and 4 will help you a lot in this step.
+     *     Example: Using "BaCdGzWZ$" as input would return the String strong
+     *     Tip! Place the return statements correctly to get the right result
+     * @param password String to be checked for spaces
+     * @return  true if there are spaces in the input String, false if there are none
+     * Points: 2
+     * New skills: building methods with methods, conditions for return statements
+     * Difficulty: 4
+     */
+
+    public static String passwordStrengthEvaluator(String password){
+        char[] symbols ={'$','%','&','£'};
+         if(passwordLengthChecker(password)){
+             if(passwordContainsSymbolChecker(password,symbols)){
+                 if(passwordContainsUpperCase(password)){
+                     return "strong";
+                 }
+                 return "medium";
+             }
+         }
+        return "weak";
+    }
+
+
+
+
+
+
+    ////////////OLD CODE/////////////////////
+
     // Write a public static function `sayHello` that returns "hello world!"
     public static String sayHello(){
         // Add Your Code Here ...
         return "hello world!";
-    }
-
-    // Write a public static function that counts how many times the letter combination "dog" appears in the string
-    public static int countDogs(String phrase) {
-        // Add Your Code Here comments
-        return -1;
     }
 }
--- src/test/java/MethodsPracticeTest.java
+++ src/test/java/MethodsPracticeTest.java
@@ -2,20 +2,212 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 class MethodsPracticeTest {
     MethodsPracticeTest() {
     }
+    // Step 1 Test
+    @DisplayName("checks if passwordLengthChecker method returns true/false for long/short passwords")
+    @Test
+    void passwordLengthCheckerTest() {
+        // Feedback in case the long password fails
+        if(MethodsPractice.passwordLengthChecker("12345678")==false){
+            fail(   "\n" +
+                    "-------------------------------------------------------------------------\n" +
+                    "AUTO-FEEDBACK:\n" +
+                    "Step 1:\n" +
+                    " Looks like your method returns false for \n" +
+                    "          8 character long passwords.\n"+
+                    "          Did you make a condition i>8?\n"+
+                    "          If so try i>=8\n"+
+                    "          If you need some examples of if statement syntax try: \n"+
+                    "          \"Introduction to Java Programming and Data Structures book\"\n" +
+                    "          chapter 3.3 if Statements\n" +
+                    "-------------------------------------------------------------------------\n"
+            );
+        }
+        // Feedback in case the short password returnd true
+        if(MethodsPractice.passwordLengthChecker("12345678")==false){
+            fail(   "\n" +
+                    "-------------------------------------------------------------------------\n" +
+                    "AUTO-FEEDBACK:\n" +
+                    "Step 1:\n" +
+                    " Your method returns true for passwords under \n" +
+                    "          8 characters long. You can check if you used the sign correctly?\n"+
+                    "          Your condition should be i>=8\n"+
+                    "          If so try i>=8\n"+
+                    "          If you need some examples of if statement syntax try: \n"+
+                    "          \"Introduction to Java Programming and Data Structures book\"\n" +
+                    "          chapter 3.3 if Statements\n" +
+                    "-------------------------------------------------------------------------\n"
+            );
+        }
+        Assertions.assertEquals(true, MethodsPractice.passwordLengthChecker("12345678"));
+        Assertions.assertEquals(false, MethodsPractice.passwordLengthChecker("12345"));
+    }
+    // Step 2 Test
+    @DisplayName("checks if stringToCharArrayTest method returns letters in password")
+    @Test
+    void stringToCharArrayTest() {
+        char [] expected = new char[]{'t', 'e', 's', 't', 's', 't', 'r', 'i', 'n', 'g'};
+        Assertions.assertArrayEquals(expected, MethodsPractice.stringToCharArray("teststring"));
+    }
+    // Step 3 Test
+    @DisplayName("checks if passwordContainsUpperCase method returns true/false correctly")
+    @Test
+    void passwordContainsUpperCaseTest() {
+        // Feedback in case the upper case letter password fails
+        if(MethodsPractice.passwordContainsUpperCase("Test")==false){
+            fail(   "\n" +
+                    "-------------------------------------------------------------------------\n" +
+                    "AUTO-FEEDBACK:\n" +
+                    "Step 3:\n" +
+                    " Looks like your method returns false for \n" +
+                    "          passwords that do contain upper case letters .\n"+
+                    "          Try to use the method Character.isUpperCase(letter)\n"+
+                    "          If you need some examples of using upper case check: \n"+
+                    "          \"Introduction to Java Programming and Data Structures book\"\n" +
+                    "          Table 4.6 Methods in the Character Class\n" +
+                    "-------------------------------------------------------------------------\n"
+            );
+        }
+        // Feedback in case the lower case password returns true
+        if(MethodsPractice.passwordContainsUpperCase("test")==true){
+            fail(   "\n" +
+                    "-------------------------------------------------------------------------\n" +
+                    "AUTO-FEEDBACK:\n" +
+                    "Step 3:\n" +
+                    " Looks like your method returns true for \n" +
+                    "          passwords that do contain only lower case letters .\n"+
+                    "          Try to use the method Character.isUpperCase(letter)\n"+
+                    "          If you need some examples of using upper case check: \n"+
+                    "          \"Introduction to Java Programming and Data Structures book\"\n" +
+                    "          Table 4.6 Methods in the Character Class\n" +
+                    "-------------------------------------------------------------------------\n"
+            );
+        }
+        // Feedback if method only checks if the first letter is upper case
+        if(MethodsPractice.passwordContainsUpperCase("teSt")==false){
+            fail(   "\n" +
+                    "-------------------------------------------------------------------------\n" +
+                    "AUTO-FEEDBACK:\n" +
+                    "Step 3:\n" +
+                    " The method needs to check all letters not only the first one \n" +
+                    "          Try testing your method with inputs that have upper\n"+
+                    "          case letters in the middle. Try using a for each loop \n"+
+                    "          to iterate through the character array of letters. \n"+
+                    "          To see an example of a for each loop see: \n"+
+                    "          \"Introduction to Java Programming and Data Structures book\"\n" +
+                    "          7.2.7 Foreach Loops\n" +
+                    "-------------------------------------------------------------------------\n"
+            );
+        }
+        Assertions.assertEquals(false, MethodsPractice.passwordContainsUpperCase("teststring"));
+        Assertions.assertEquals(true, MethodsPractice.passwordContainsUpperCase("tesTstring"));
+    }
 
+    // Step 4 Test
+    @DisplayName("checks if passwordContainsSymbolChecker method returns true/false correctly")
+    @Test
+    void passwordContainsSymbolCheckerTest() {
+        // if method fails to identify that password contains symbols
+        if(MethodsPractice.passwordContainsSymbolChecker("Pa$$word123",new char[]{'$','%','&'})==false){
+            fail(   "\n" +
+                    "-------------------------------------------------------------------------\n" +
+                    "AUTO-FEEDBACK:\n" +
+                    "Step 4:\n" +
+                    " The method failed to identify that password contains special symbols '$','%','&'\n" +
+                    "          Try using nested for each loops to check if each\n"+
+                    "          letter from password letters char[] \n"+
+                    "          matches a symbol from the symbols char array \n"+
+                    "          To do this you can use nested loops: \n"+
+                    "          \"Introduction to Java Programming and Data Structures book\"\n" +
+                    "          5.9 Nested Loops\n" +
+                    "-------------------------------------------------------------------------\n"
+            );
+        }
+        // if method fails to identify that password does not contain symbols
+        if(MethodsPractice.passwordContainsSymbolChecker("Password123",new char[]{'$','%','&'})==true){
+            fail(   "\n" +
+                    "-------------------------------------------------------------------------\n" +
+                    "AUTO-FEEDBACK:\n" +
+                    "Step 4:\n" +
+                    " The method failed to identify that password does not contain special symbols '$','%','&'\n" +
+                    "          Try using nested for each loops to check if each\n"+
+                    "          letter from password letters char[] \n"+
+                    "          matches a symbol from the symbols char array \n"+
+                    "          To do this you can use nested loops: \n"+
+                    "          \"Introduction to Java Programming and Data Structures book\"\n" +
+                    "          5.9 Nested Loops\n" +
+                    "-------------------------------------------------------------------------\n"
+            );
+        }
+        Assertions.assertEquals(true, MethodsPractice.passwordContainsSymbolChecker("Pa$$word123",new char[]{'$','%','&'}));
+        Assertions.assertEquals(false, MethodsPractice.passwordContainsSymbolChecker("Password123",new char[]{'$','%','&'}));
+    }
+
+    // Step 5 Test
+    @DisplayName("checks if passwordStrengthEvaluator method returns weak/medium/strong correctly")
+    @Test
+    void passwordStrengthEvaluatorTest() {
+        // checks if return Strings are upper case
+        if(MethodsPractice.passwordStrengthEvaluator("123").equals("Weak")||MethodsPractice.passwordStrengthEvaluator("pa$$word123").equals("Medium")){
+            fail(   "\n" +
+                    "-------------------------------------------------------------------------\n" +
+                    "AUTO-FEEDBACK:\n" +
+                    "Step 4:\n" +
+                    " The solution is almost correct!\n" +
+                    "          Please check your return types\n"+
+                    "          they must be lower case\n"+
+                    "          weak, medium, strong \n"+
+                    "-------------------------------------------------------------------------\n"
+            );
+        }
+        // if method fails to identify that password contains
+        if(MethodsPractice.passwordStrengthEvaluator("pa$$word123").equals("weak")){
+            fail(   "\n" +
+                    "-------------------------------------------------------------------------\n" +
+                    "AUTO-FEEDBACK:\n" +
+                    "Step 4:\n" +
+                    " The method failed to identify that password contains a special symbol\n" +
+                    "          If your passwordContainsSymbolChecker from step 4\n"+
+                    "          works correctly, the placement of return statements could be the problem.\n"+
+                    "          Try moving your return medium statement \n"+
+                    "          inside the passwordContainsSymbolChecker(password,symbols)\n"+
+                    "          condition code block \n"+
+                    "          You can read more about code blocks in: \n"+
+                    "          \"Introduction to Java Programming and Data Structures book\"\n" +
+                    "          1.7 A Simple Java Program inside blocks section\n" +
+                    "-------------------------------------------------------------------------\n"
+            );
+        }
+        // if method fails to identify that password contains
+        if(MethodsPractice.passwordStrengthEvaluator("Pa$$word123").equals("medium")){
+            fail(   "\n" +
+                    "-------------------------------------------------------------------------\n" +
+                    "AUTO-FEEDBACK:\n" +
+                    "Step 4:\n" +
+                    " The method failed to identify that password contains a special symbol\n" +
+                    "          If your passwordContainsUpperCase from step 3\n"+
+                    "          works correctly, the placement of return statements could be the problem.\n"+
+                    "          Try moving your return medium statement \n"+
+                    "          inside the passwordContainsUpperCase(password,symbols)\n"+
+                    "          condition code block \n"+
+                    "          You can read more about code blocks in: \n"+
+                    "          \"Introduction to Java Programming and Data Structures book\"\n" +
+                    "          1.7 A Simple Java Program inside blocks section\n" +
+                    "-------------------------------------------------------------------------\n"
+            );
+        }
+        Assertions.assertEquals("weak", MethodsPractice.passwordStrengthEvaluator("1234"));
+        Assertions.assertEquals("medium", MethodsPractice.passwordStrengthEvaluator("pa$$word123"));
+        Assertions.assertEquals("strong", MethodsPractice.passwordStrengthEvaluator("Pa$$word123"));
+    }
     @DisplayName("checks if sayHello method returns String hello world")
     @Test
     void Should_ReturnHelloWorld() {
         Assertions.assertEquals("hello world!", MethodsPractice.sayHello());
     }
 
-    @DisplayName("countDogs no dogs in string")
-    @Test
-    void Should_ReturnZero_When_NoDogInString() {
-        int count = MethodsPractice.countDogs("cat cat catcat");
-        Assertions.assertEquals(0, count);
-    }
 }
